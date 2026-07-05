31453086e4e461c7fdcfe10d5a9c6a6023c64627
r-on-s <ronyarif700@gmail.com>
2026-07-05 15:36:13 +0600

    django template language

--- newapp/views.py
+++ newapp/views.py
@@ -10,5 +10,5 @@ def About(request):
     return HttpResponse('This is about page.')
 
 def Index(request):
-    data={'name':'karim','age':20}
+    data={'name':'karim','age':18}
     return render(request,'index.html',data)
