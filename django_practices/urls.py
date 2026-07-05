ce79dfa41244db90c10b5f3257cf7f3cfa86f5fc
r-on-s <ronyarif700@gmail.com>
2026-07-05 15:02:40 +0600

    render templates with globally

--- django_practices/urls.py
+++ django_practices/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Home),
+    path('index/',views.Index),
     path('first_app/',include('newapp.urls'))
 ]
--- django_practices/views.py
+++ django_practices/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def Home(request):
     return HttpResponse("This is home page")
+
+
+def Index(request):
+    return render(request,'index.html')
