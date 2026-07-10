94323901044b60beae803aa914b7c842e5c3ff47
r-on-s <ronyarif700@gmail.com>
2026-07-10 19:53:45 +0600

    built in form validators with user defined function and file extension allwed in built in form

--- newapp/forms.py
+++ newapp/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from django.core import validators
 
+
+
+def len_check(value):
+    if len(value) < 10:
+        raise forms.ValidationError("Enter a vlaue with at least 10 chars")
 class ContactForm(forms.Form):
-    name=forms.CharField(widget=forms.TextInput,validators=[validators.MinLengthValidator(10,message='Enter a name with at least 10 chars')])
+    name=forms.CharField(widget=forms.TextInput,validators=[len_check])
     email=forms.CharField(widget=forms.EmailInput)
     age=forms.IntegerField(validators=[validators.MinValueValidator(18,message='Enter a age above 18')])
+    file=forms.FileField(validators=[validators.FileExtensionValidator(allowed_extensions=['pdf','svg'])])
 
         
 
