cdad421e54c6c82b0b57cb438a62f6baa3a68df7
r-on-s <ronyarif700@gmail.com>
2026-07-10 19:35:02 +0600

    cleaning a validation form data in single function

--- newapp/forms.py
+++ newapp/forms.py
@@ -3,15 +3,14 @@ from django import forms
 class ContactForm(forms.Form):
     name=forms.CharField(widget=forms.TextInput)
     email=forms.CharField(widget=forms.EmailInput)
-    def clean_email(self):
+    def clean(self):
+        self.cleaned_data=super().clean()
+        name=self.cleaned_data['name']
         email=self.cleaned_data['email']
+        if len(name) <10:
+            raise forms.ValidationError("Name should be at least 10 chars")
         if '.com' not in email:
             raise forms.ValidationError("Email should contain .com")
-        return email
-    def clean_name(self):
-        name=self.cleaned_data['name']
-        if len(name) < 10:
-            raise forms.ValidationError("Name should be at least 10 chars")
-        return name
 
+        
 
