be629e10c231972165df71140bb52167d52ae11c
r-on-s <ronyarif700@gmail.com>
2026-07-10 12:34:38 +0600

    taking file from user and uploading it

--- newapp/forms.py
+++ newapp/forms.py
@@ -2,9 +2,4 @@ from django import forms
 
 class ContactForm(forms.Form):
     name=forms.CharField()
-    email=forms.EmailField()
-    age=forms.IntegerField()
-    CHOICES = [('s','small'),('m','medium'),('l','large')]
-    size=forms.ChoiceField(choices=CHOICES)
-    MEAL = [('p','pepparoni'),('m','mashrom'),('b','beef')]
-    pizaa=forms.MultipleChoiceField(choices=MEAL)
+    file=forms.FileField()
--- newapp/views.py
+++ newapp/views.py
@@ -37,8 +37,18 @@ def user_form(request):
 
 
 def Django_form(request):
-    form=ContactForm(request.POST)
-    if form.is_valid():
-        print(form.cleaned_data)
-    return render(request,'django_form.html',{'form':form})
+    if request.method=='POST':
+        form=ContactForm(request.POST,request.FILES)
+        if form.is_valid():
+            file=form.cleaned_data['file']
+            with open('./newapp/upload/'+file.name,'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+
+            print(form.cleaned_data)
+            return render(request,'django_form.html',{'form':form})
+    else:
+        form=ContactForm()
+        return render(request,'django_form.html',{'form':form})
+
           
