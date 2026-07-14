bbd65a3c8a44ad17eea620697748985775030577
r-on-s <ronyarif700@gmail.com>
2026-07-14 06:10:25 +0600

    form value extra design with widget and showing errors fields

--- newapp/forms.py
+++ newapp/forms.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 class UserCreate(UserCreationForm):
+    email=forms.EmailField(widget=forms.EmailInput(attrs={'id':'required'}))
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
