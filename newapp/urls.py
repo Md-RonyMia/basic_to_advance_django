c0a251c67cfc61e1ee802fe64e064404aaa0f0df
r-on-s <ronyarif700@gmail.com>
2026-07-14 12:51:15 +0600

    change passwor using old password

--- newapp/urls.py
+++ newapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
        path('login/',views.user_login,name='login'),
        path('profile/',views.profile,name='profile'),
        path('logout/',views.user_logout,name='logout'),
+       path('pass_change/',views.pass_change,name='pass_change'),
 
     
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import UserCreate
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm,SetPasswordForm
+from django.contrib.auth import authenticate, login,logout,update_session_auth_hash
 
 
 def sign_up(request):
@@ -45,3 +45,15 @@ def profile(request):
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+
+def pass_change(request):
+    if request.method=='POST':
+        form=PasswordChangeForm(user=request.user,data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request,form.user)
+            return redirect('profile')
+    else:
+        form=PasswordChangeForm(user=request.user)
+    return render(request,'passchange.html',{'form':form})
